1b813897b6175897e5e9de848772842819304517
KhanhDV <dangvankhanh09022003@gmail.com>
2026-02-03 09:03:03 +0700

    firebase ssvid

--- apps/ssvid.cc/src/libs/firebase/firebase-config.ts
+++ apps/ssvid.cc/src/libs/firebase/firebase-config.ts
@@ -5,11 +5,11 @@
  */
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDSmVIIT-HEFl5ALY-1MSA7pA8jtWKyLa4",
-  authDomain: "ytmp3my-d6bec.firebaseapp.com",
-  projectId: "ytmp3my-d6bec",
-  storageBucket: "ytmp3my-d6bec.firebasestorage.app",
-  messagingSenderId: "528513706848",
-  appId: "1:528513706848:web:d41fd9670de87a08788c05",
-  measurementId: "G-X53D7RMZMD"
+  apiKey: "AIzaSyB3AllYBhQaCOtzIh7kaymm4q-lIBDktqQ",
+  authDomain: "ssivdcc.firebaseapp.com",
+  projectId: "ssivdcc",
+  storageBucket: "ssivdcc.firebasestorage.app",
+  messagingSenderId: "866971516017",
+  appId: "1:866971516017:web:950b13cacc925c0b372fd0",
+  measurementId: "G-C8F8XDW85Q"
 };
