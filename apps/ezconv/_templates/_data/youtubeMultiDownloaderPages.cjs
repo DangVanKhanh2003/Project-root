d8aeb52a4b190e1bbe5d1d720f3173c135f42199
KhanhDV <dangvankhanh09022003@gmail.com>
2026-02-25 15:26:32 +0700

    clean

--- apps/ezconv/_templates/_data/youtubeMultiDownloaderPages.cjs
+++
@@ -1,9 +0,0 @@
-const allPages = require('./allPages.cjs');
-
-/**
- * Filter allPages to only return youtube-multi-downloader page variants
- */
-module.exports = function () {
-  const pages = allPages();
-  return pages.filter(p => p.pageKey === 'youtube-multi-downloader');
-};
