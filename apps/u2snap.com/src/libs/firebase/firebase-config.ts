3d1d462eb61b8ee1e060295390f92bfcce1ce378
KhanhDV <dangvankhanh09022003@gmail.com>
2026-03-17 15:10:04 +0700

    firebase u2snap

--- apps/u2snap.com/src/libs/firebase/firebase-config.ts
+++ apps/u2snap.com/src/libs/firebase/firebase-config.ts
@@ -5,5 +5,11 @@
  */
 
 export const firebaseConfig = {
-
+  apiKey: "AIzaSyDjjomGcWoGxhQv8q8hpkEeCFyXjNw0dCM",
+  authDomain: "u2snap.firebaseapp.com",
+  projectId: "u2snap",
+  storageBucket: "u2snap.firebasestorage.app",
+  messagingSenderId: "107260445598",
+  appId: "1:107260445598:web:66452b25836e9cf83d4c8f",
+  measurementId: "G-KJRS3E3323"
 };
