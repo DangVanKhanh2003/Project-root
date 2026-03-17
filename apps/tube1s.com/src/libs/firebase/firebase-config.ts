b5e27306eb3e26432e8543637c336a2b09d376d2
KhanhDV <dangvankhanh09022003@gmail.com>
2026-03-17 15:36:09 +0700

    firebase config tube1s

--- apps/tube1s.com/src/libs/firebase/firebase-config.ts
+++ apps/tube1s.com/src/libs/firebase/firebase-config.ts
@@ -5,5 +5,11 @@
  */
 
 export const firebaseConfig = {
-
+  apiKey: "AIzaSyBg2xNOyNjc7WDMx-wCv5I9MARU2jKOjN4",
+  authDomain: "tube1s.firebaseapp.com",
+  projectId: "tube1s",
+  storageBucket: "tube1s.firebasestorage.app",
+  messagingSenderId: "110436874913",
+  appId: "1:110436874913:web:8c185df9b5a53c4d5ccee0",
+  measurementId: "G-7GEPCKP6RW"
 };
